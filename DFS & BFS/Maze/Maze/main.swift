a27199510cb6b5effafaa2154956a53484f63424
YeongHyeon <joyh0808@gmail.com>
2024-03-20 19:34:48 +0900

    백준 2178: 시간초과 해결
    BFS에서 노드 방문여부를 배열을 만들어서 해당 위치에 값을 저장하는 식으로 contains을 제거하였다.

--- DFS & BFS/Maze/Maze/main.swift
+++ DFS & BFS/Maze/Maze/main.swift
@@ -11,6 +11,7 @@ let input = readLine()!.split(separator: " ").map { Int($0)! }
 let N: Int = input[0]
 let M: Int = input[1]
 var array = [[Int]](repeating: [], count: N)
+var visited = [[Bool]](repeating: [Bool](repeating: false, count: M), count: N)
 
 for i in 0..<N {
     let input = readLine()!.map { Int(String($0))! }
@@ -20,35 +21,28 @@ for i in 0..<N {
 let udlr = [[0, -1], [0, 1], [-1, 0], [1, 0]] // 상, 하, 좌, 우
 
 // 최소 이동을 확인하기 위해 BFS를 이용한다.
-func maze(_ maze: [[Int]]) -> Int {
-    var visitedQueue: [[Int]] = []
+func maze(_ maze: [[Int]]){
     var needVisitQueue: [[Int]] = [[0, 0, 1]]
+    visited[0][0] = true
     
     while !needVisitQueue.isEmpty {
         let node = needVisitQueue.removeFirst()
         
         if node[0] == N-1 && node[1] == M-1 {
-            return node[2]
+            print(node[2])
+            break
         }
-        
-        if visitedQueue.contains(node) {
-            continue
-        }
-        
-        visitedQueue.append(node)
         
         for i in 0..<udlr.count {
             let x = node[1] + udlr[i][0]
             let y = node[0] + udlr[i][1]
             
-            if x >= 0 && x < M && y >= 0 && y < N && maze[y][x] == 1 {
+            if x >= 0 && x < M && y >= 0 && y < N && maze[y][x] == 1 && !visited[y][x] {
+                visited[y][x] = true
                 needVisitQueue.append([y, x, node[2] + 1])
             }
         }
     }
-    
-    return visitedQueue.count
 }
 
-let result: Int = maze(array)
-print(result)
+maze(array)
