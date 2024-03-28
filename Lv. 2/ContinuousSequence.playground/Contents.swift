851563c3ce37f18fa5fe5b25bde7ae530f9a9f3e
YeongHyeon <joyh0808@gmail.com>
2024-03-28 16:54:37 +0900

    Lv 2. 연속된 부분 수열의 합
    문제 해결완료.
    이중 for문이였기 때문에 시간초과가 에러가 되었다.
    해결방법은 범위를 지정하고, 조건을 만족하는 경우 범위를 조절하였다.

--- Lv. 2/ContinuousSequence.playground/Contents.swift
+++ Lv. 2/ContinuousSequence.playground/Contents.swift
@@ -1,36 +1,83 @@
 import Foundation
 
+/*
+ var result: [Int] = []
+ var sum: Int = 0
+ var sumIndex: [Int] = []
+ 
+ for index in 0..<sequence.count {
+ sum = sum + sequence[index]
+ sumIndex.append(index)
+ 
+ print("index: \(index)")
+ print("sum: \(sum)")
+ print("sumIndex: \(sumIndex)")
+ 
+ if sum == k {
+ if result.isEmpty {
+ result = sumIndex
+ } else if result.count > sumIndex.count {
+ result = sumIndex
+ }
+ } else if sum < k {
+ continue
+ } else if sum > k {
+ sum -= sequence[sumIndex.first!]
+ sumIndex.removeFirst()
+ }
+ }
+ */
+
 func solution(_ sequence:[Int], _ k:Int) -> [Int] {
     var result: [Int] = []
-    var sum: Int = 0
-    var sumIndex: [Int] = []
+    // index 값
+    var left: Int = 0
+    var right: Int = 0
     
-    for index in 0..<sequence.count {
-        for j in index..<sequence.count {
-            sum = sum + sequence[j]
-            sumIndex.append(j)
-            
-            print("index: \(index)")
-            print("sum: \(sum)")
-            print("sumIndex: \(sumIndex)")
-            
-            if sum == k {
-                if result.isEmpty {
-                    result = sumIndex
-                } else if result.count > sumIndex.count {
-                    result = sumIndex
-                }
-            } else if sum < k {
-                continue
-            } else if sum > k {
-                sum = 0
-                sumIndex = []
-                break
+    var sum = sequence[left]
+    
+    while left < sequence.count - 1 {
+        print("left: \(left), right: \(right), sum: \(sum)")
+        if sum < k {
+            if right < sequence.count - 1 {
+                right += 1
+                sum += sequence[right]
+            } else {
+                sum -= sequence[left]
+                left += 1
             }
+        } else if sum > k {
+            sum -= sequence[left]
+            left += 1
+        }
+        
+        if sum == k {
+            print(result.isEmpty)
+            if result.isEmpty {
+                result = [left, right]
+            } else {
+                print("check")
+                print(result[1] - result[0])
+                print(right - left)
+                if (result[1] - result[0]) > (right - left) {
+                    result = [left, right]
+                } else {
+                    sum -= sequence[left]
+                    left += 1
+                }
+            }
+            
+            if right < sequence.count - 1 {
+                right += 1
+                sum += sequence[right]
+            }
+            
         }
     }
     
-    return [result.first!, result.last!]
+    print("result: \(result)")
+    
+    return result
 }
 
 let sequence1 = [1, 2, 3, 4, 5]
@@ -42,6 +89,9 @@ let k2 = 5
 let sequence3 = [2, 2, 2, 2, 2]
 let k3 = 6
 
-print("test1: \(solution(sequence1, k1))")
-print("test2: \(solution(sequence2, k2))")
-print("test3: \(solution(sequence3, k3))")
+solution(sequence1, k1)
+
+solution(sequence2, k2)
+
+solution(sequence3, k3)
+
